8c2e5d8f42840891e0823bebde6080d425d8c28d
Tanner Williams <tannerwill756@gmail.com>
2020-03-12 16:31:54 -0400

    add my card, working on followers cards

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,12 +3,66 @@
            https://api.github.com/users/<your name>
 */
 
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
 
    Skip to Step 3.
 */
+
+axios.get('https://api.github.com/users/Tannerwill756')
+  .then(response => {
+    console.log(response.data);
+    const myInfo = myNewCard(response.data);
+    const cards = document.querySelector('.cards');
+    cards.append(myInfo);
+  })
+  .catch(error => {
+    console.log('You are getting the error:', error);
+  })
+
+function myNewCard (gitHubInfo) {
+  const parentCard = document.createElement('div'),
+        userImg = document.createElement('img'),
+        cardInfo = document.createElement('div'),
+        nameHeader = document.createElement('h3'),
+        userName = document.createElement('p'),
+        location = document.createElement('p'),
+        profile = document.createElement('p'),
+        myUrl = document.createElement('a'),
+        followers = document.createElement('p'),
+        following = document.createElement('p'),
+        myBio = document.createElement('p');
+
+        parentCard.append(userImg);
+        parentCard.append(cardInfo);
+        cardInfo.append(nameHeader);
+        cardInfo.append(userName);
+        cardInfo.append(location);
+        cardInfo.append(profile);
+        profile.append(myUrl);
+        cardInfo.append(followers);
+        cardInfo.append(following);
+        cardInfo.append(myBio);
+
+        parentCard.classList.add('card');
+        cardInfo.classList.add('card-info');
+        nameHeader.classList.add('name')
+        userName.classList.add('username');
+
+        userImg.src = gitHubInfo.avatar_url;
+        nameHeader.textContent = gitHubInfo.name;
+        userName.textContent = gitHubInfo.login;
+        location.textContent = gitHubInfo.location;
+        myUrl.href = gitHubInfo.html_url;
+        myUrl.textContent = gitHubInfo.html_url;
+        followers.textContent = `Followers: ${gitHubInfo.followers}`;
+        following.textContent = `Following: ${gitHubInfo.following}`;
+        myBio.textContent = `Bio: ${gitHubInfo.bio}`;
+
+  return parentCard;
+}
 
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
@@ -24,7 +78,22 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
+
+axios.get('https://api.github.com/users/Tannerwill756/followers')
+  .then(response =>{
+    // console.log(response.data);
+    console.log(response.data);
+    const followersArray = myNewCard(response.data);
+    const cards = document.querySelector('.cards');
+    cards.append(followersArray);
+
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+  // console.log(followersArray);
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -53,3 +122,6 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+
+
